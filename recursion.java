d8c803d65ee39d8a322de8916f4733ba07655adf
hassandiab121 <51037673+hassandiab121@users.noreply.github.com>
2022-04-23 12:51:50 -0700

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignments;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class recursion {
+
+    /**
+     * @param args the command line arguments
+     */
+    static char s[]={'h','a','s','s','a','n'};
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+      reverseString(s);
+      int counter=0;
+     while(counter !=6)
+     {
+      System.out.print(s[counter]+" ");
+      counter++;
+     }
+       
+    }
+    
+    public static  void reverseString(char[] s) {
+     int start =0;
+     int end =s.length -1;
+ 
+       reverseString(s , start, end); // overloaded recursive helper function
+       
+    }
+ 
+    private static void reverseString(char[] s, int start, int end ) // recursive helper function
+    {
+ 
+        if(start == end || start > end ) // base case 
+            return;
+        else
+        {
+ 
+            char m =s[start];
+            s[start]=s[end];
+            s[end]=m;
+            reverseString(s,++start,--end);
+ 
+        }
+    } 
+      
+    
+    
+    
+    
+}
